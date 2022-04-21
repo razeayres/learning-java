767e70ce6167860a7f1c9c4fdcaf9243f25372d1
Rodrigo Miranda <rodrigo.qmiranda@gmail.com>
2022-04-21 17:29:19 -0400

    Add comments

--- learning-java/src/main/java/building_test/ImportLibrary.java
+++ learning-java/src/main/java/building_test/ImportLibrary.java
@@ -5,27 +5,50 @@ import static my_lib.math_utils.mult; // statically importing a method
 
 public class ImportLibrary {
     public static void main(String[] args) {
-        class_approach();
-        static_approach();
-    }
-
-    public static void static_approach() {
+        /*
+        here we declare and
+        define the variables
+        a and b
+        */
         double a = 4;
         double b = 25;
 
-        double result = mult(a, b);
+//        this to excecute our diffrent approaches
+        class_approach(a, b);
+        static_approach(a, b);
+        class_approach2(a, b);
+    }
+
+//    this just used the imported static method
+    public static void static_approach(double a, double b) {
+        double result = mult(a, b); // calls the method 'mult'
 
         System.out.println(result);
     }
 
-    public static void class_approach() {
-        double a = 4;
-        double b = 25;
+    /*
+    this creates a class calc
+    of type math_utils,
+    and uses it method
+    */
+    public static void class_approach(double a, double b) {
+        math_utils calc; // declares the object calc as type math_utils
+        calc = new math_utils(); // defines the variable as a new object
 
-        math_utils calc;
-        calc = new math_utils();
+        double result = calc.mult(a, b); // calls the method 'mult'
 
-        double result = calc.mult(a, b);
+        System.out.println(result);
+    }
+
+    /*
+    this is the same thing
+    as the previous one,
+    but here we do not
+    have to declare and
+    define the math_utils object
+    */
+    public static void class_approach2(double a, double b) {
+        double result = math_utils.mult(a, b); // calls the method 'mult' directly by: class.method
 
         System.out.println(result);
     }
